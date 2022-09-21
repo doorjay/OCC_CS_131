fc627382b8c40a0d2d82d0b956553c365f79b5a5
doorjay <dorjekmp@gmail.com>
2022-09-21 16:51:15 -0700

    blank

--- CS131_week04/LabExercises02/lab03.py
+++ CS131_week04/LabExercises02/lab03.py
@@ -3,3 +3,10 @@
 # Python 1, CS 131
 # Mess with the string Hello World
 
+str = "  Hello, World  "
+
+print("The number of l's is", str.count('l'))
+print("***" + str.strip() + "***")
+print("***" + str.lstrip() + "***")
+print("***" + str.rstrip() + "***")
+print(str.strip().replace("o", "***"))
